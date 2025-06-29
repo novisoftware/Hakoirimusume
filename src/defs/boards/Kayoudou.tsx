a80f8cdfae65e3ffa11ddde5f9ec0c960a345606
novisoftware <sh.sado@gmail.com>
2025-06-29 15:12:19 +0900

    パズルのバリエーションを追加

--- src/defs/boards/Kayoudou.tsx
+++ src/defs/boards/Kayoudou.tsx
@@ -8,13 +8,11 @@ const DEFINITION_KAYOUDOU: BoardDefinition = {
         { id: 1, label: "黄忠", size: { width: 1, height: 2 }, position: { x: 1, y: 1 } },
         { id: 2, label: "曹操", size: { width: 2, height: 2 }, position: { x: 2, y: 1 } },
         { id: 3, label: "張飛", size: { width: 1, height: 2 }, position: { x: 4, y: 1 } },
-
         { id: 4, label: "兵", size: { width: 1, height: 1 }, position: { x: 1, y: 3 } },
         { id: 5, label: "馬超", size: { width: 1, height: 2 }, position: { x: 1, y: 4 } },
         { id: 6, label: "関羽", size: { width: 2, height: 1 }, position: { x: 2, y: 3 } },
         { id: 7, label: "趙雲", size: { width: 1, height: 2 }, position: { x: 4, y: 3 } },
         { id: 9, label: "兵", size: { width: 1, height: 1 }, position: { x: 4, y: 5 } },
-
         { id: 8, label: "兵", size: { width: 1, height: 1 }, position: { x: 2, y: 4 } },
         { id: 9, label: "兵", size: { width: 1, height: 1 }, position: { x: 3, y: 4 } },
     ]
